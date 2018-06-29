3a3ffcaa7e19fcddc9af7e442500787c018b4a3d
Dasari Kamalson <dasarikamalson@gmail.com>
2018-06-29 20:18:25 +0530

    Upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/main.css">
+    <title>Pastor Karuna Babu</title>
+</head>
+<body>
+<!--navigation bar starts-->
+<div class="menu">
+    <nav>
+        <ul>
+        <span style="color:black; margin-right:280px; padding-left:8px;">
+        <a href="/pkb/index.php" style="text-decoration: none; color:black; font-weight: bold;" >KARUNA BABU</a>
+        </span>
+            <li><a href="#" class="one">Home</a></li>
+            <li><a href="#"class="two">Menu</a></li>
+            <li><a href="#"class="three">Menu</a></li>
+            <li><a href="#"class="four">Menu</a></li>
+            <li><a href="#"class="one">Menu</a></li>
+            <li><a href="#"class="two">Menu</a></li>
+            <li><a href="#"class="three">Menu</a></li>
+            <li><a href="#"class="four">Menu</a></li>
+            <span style="padding-left:130px;">
+            <li><a href="#" class="fa fa-facebook" style="color: #364f6b; "></a></li>
+            <li><a href="#" class="fa fa-twitter" style="color: #364f6b; "></a></li>
+            <li><a href="#" class="fa fa-youtube" style="color: #364f6b; "></a></li>
+            </span>
+        </ul>
+    </nav>
+</div>
+<!--Nav Bar Ends-->
+
+<!--slider starts-->
+<section class="slider1">
+<!-- particles.js container --> 
+<div id="particles-js"></div>
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script> 
+<script  src="js/index.js"></script>
+</section>
+
+<!--slider ends-->
+
+<section class="slider2">
+
+</section>
+
+<section class="slider3">
+
+</section>
+
+<!--footer Starts-->
+    <div class="footer">
+  <p>Copyright © 2000-2018. | All Rights Reserved. | <a href="karunababu.com" title="© Karuna Babu" style="text-decoration:none; color: #eeeeee;">www.karunababu.com</a></p>
+    </div>
+</body>
+</html>
+
+
